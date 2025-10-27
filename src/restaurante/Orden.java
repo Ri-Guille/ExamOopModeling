6b99dde0d9b89f438c9b019fcc1f6d14268c2513
Ri-Guille <g.riverorebanal@colegiovirgendeeuropa.edu.es>
2025-10-27 13:35:07 +0100

    Quinto Commit Orden code

--- src/restaurante/Orden.java
+++ src/restaurante/Orden.java
@@ -3,9 +3,26 @@
  */
 package restaurante;
 
+import java.time.LocalTime;
+
 /**
  * @author G 27 oct 2025
  */
 public class Orden {
+    private static int contadorId = 1;
+    private final int idOrden;
+    private final Plato plato;
+    private final LocalTime hora;
 
+    public Orden(Cliente cliente, Plato plato) {
+        this.idOrden = contadorId++;
+        this.plato = plato;
+        this.hora = LocalTime.now().withNano(0);
+    }
+
+    public void imprimirTicket() { // SALIDA
+        System.out.println("\n--- SALIDA (Ticket de Orden #" + idOrden + ") ---");
+        System.out.println("Plato: " + plato.getNombre());
+        System.out.println("Hora: " + hora);
+    }
 }
