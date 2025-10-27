0e3d165f86055aceb863f0a7f4f56e95e5248cfc
Aleeex3 <a.garciasanz@colegiovirgendeeuropa.edu.es>
2025-10-27 13:38:51 +0100

    SEXTO COMMIT

--- src/restaurante/Mesero.java
+++ src/restaurante/Mesero.java
@@ -7,5 +7,19 @@ package restaurante;
  * @author G 27 oct 2025
  */
 public class Mesero {
+    private final String nombre;
 
+    public Mesero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // ENTRADA & PROCESO: Toma pedido, lo registra en Cuenta y genera Orden
+    public Orden tomarPedido(Cliente cliente, Plato plato, Cuenta cuenta) {
+        System.out.println("\n--- ENTRADA ---");
+        System.out.println(nombre + " toma el pedido de " + cliente.getNombre() + ": " + plato.getNombre());
+        
+        cuenta.agregarItem(plato); // Proceso en Cuenta
+        
+        return new Orden(cliente, plato); // Genera la Orden
 }
+    }
